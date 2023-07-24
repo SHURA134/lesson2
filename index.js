9476f9a2fe0dd59a343692f013e113f5bfb7d11d
SHURA134 <sashamor1313@gmail.com>
2023-07-24 22:52:01 +0300

    lesson2

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
 const express= require (`express`);
-const bodyParser= require(`body-parser`);
-
-
 const {footballersRouter} = require('./routes/footballersRouter.js');
-const {goalsScoredRouter} = require(`./routes/goalsScoredRouter.js`);
-const {playerInfo} = require(`./routes/playerInfo.js`);
-const {winnerZm} = require(`./routes/mostZmWinnerRouter.js`);
-
-
-const {youngPlayer} = require(`./routes/youngPlayerRouter.js`);
-
-
 
 
 const app = express();
-app.use(bodyParser.json());
 app.use(`/footballers`,footballersRouter);
-app.use(`/goalsScored`,goalsScoredRouter);
-app.use(`/playerInfo`,playerInfo);
-app.use(`/zmWinner`,winnerZm);
-app.use(`/youngPlayer`,youngPlayer);
-
-
-
 
 
 const port=3002;
--- routes/footballersRouter.js
+++ routes/footballersRouter.js
@@ -4,17 +4,90 @@ const footballersController = require('../controlers/footballersController');
 const footballersRouter  = express.Router();
 
 
-
-
 footballersRouter.get('/', async (request, response) => {
-    const footballersUnicList=await footballersController.getUnicFootballers();
-    response.send(footballersUnicList);
-
-
+    const footballersList=await footballersController.getFootballers();
+    response.send(footballersList.data);
 });
 
 
 
+footballersRouter.get('/allWinners', async (request, response) => {
+    const footballersUnicList=await footballersController.getUnicFootballers();
+    response.send(footballersUnicList);
+});
+
+
+footballersRouter.get(`/mostGoals`,async (request,response) =>{
+
+    const goals = await footballersController.getFootballers();
+    let goalsScored = 0, goalsScoredId = 0;
+
+
+    for(let i=0;i<goals.data.length;i++){
+        if(goals.data[i].goalsScored>goalsScored) {
+            goalsScored=goals.data[i].goalsScored;
+            goalsScoredId = i;
+        };
+    }
+
+    response.send(`top-1 forward: ${goals.data[goalsScoredId].player} goals scored: ${goals.data[goalsScoredId].goalsScored}` );
+})
+
+
+footballersRouter.get(`/youngPlayer`,async (request,response) => {
+    const footballersList = await footballersController.getFootballers();
+    let age = new Array();
+
+
+    footballersList.data.forEach(elem => {
+        let bd = elem.dateOfBirth.split('-');
+        if (age.length === 0) {
+            age = bd;
+            age.push(elem.player);
+        } else if (bd[0] > age[0] || bd[0] === age[0] && bd[1] > age[1] || bd[0] === age[0] && bd[1] === age[1] && bd[2] > age[2]) {
+            age = bd;
+            age.push(elem.player);
+        }
+
+    })
+
+
+    response.send(age.reverse());
+});
+
+footballersRouter.get(`/mostWinner`, async (request,response)  =>{
+    const footballersList= await footballersController.getFootballers();
+    const footballersUnicList=await footballersController.getUnicFootballers();
+    let lenWin=0;
+    let winner;
+
+
+
+    footballersUnicList.forEach((item) =>{
+        if(footballersList.data.filter(elem => (item===elem.player)).length>lenWin){
+            lenWin=footballersList.data.filter(elem => (item===elem.player)).length;
+            winner=item;
+        }
+    })
+
+    //я побачив як зробити краще, але жінка чекає мене дивитись серіал(((
+    response.send(`${winner} won ${lenWin} times`);
+
+})
+
+
+footballersRouter.get(`/PlayerInfo`, async (request,response) =>{
+    const playersList= await footballersController.getFootballers();
+    const playerSearch=request.query.player;
+    let getPlayerInfo= new Array();
+
+
+    playersList.data.forEach((elem,i,arr) =>{
+        if( elem.player.toLowerCase().includes(playerSearch))     getPlayerInfo.push(elem);
+        else if(i===arr.length-1 && getPlayerInfo.length===0)  response.send(404);
+        else if(i===arr.length-1 && getPlayerInfo.length>0)  response.send(getPlayerInfo);
+    })
+});
 
 
 module.exports= {footballersRouter};
