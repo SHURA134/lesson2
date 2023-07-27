6af7a1be5d7d9c2196a4a2448373ac029fc3a2fa
SHURA134 <sashamor1313@gmail.com>
2023-07-27 18:38:50 +0300

    (lesson2)

--- controlers/footballersController.js
+++ controlers/footballersController.js
@@ -1,12 +1,13 @@
 const axios = require('axios');
 const fs = require('fs');
 
-
+//отримуємо масив об'єктів
 async function getFootballers() {
     const response = await axios.get('https://run.mocky.io/v3/093c503b-5a55-4764-be41-6f4d3a8eb32b');
     return response;
 }
 
+//отримуємо масив унікальних переможців ЗМ
 async function getUnicFootballers() {
     const response = await axios.get('https://run.mocky.io/v3/093c503b-5a55-4764-be41-6f4d3a8eb32b');
     let players=new Array();
@@ -16,5 +17,60 @@ async function getUnicFootballers() {
     return players;
 }
 
+//отримуємо всю інформацюю про заданого футболіста
+function playerInfo(arr,str) {
+    let getPlayerInfo= new Array();
 
-module.exports={getFootballers,getUnicFootballers};
+    arr.forEach((elem,i,arr1) =>{
+        if( elem.player.toLowerCase().includes(str))    getPlayerInfo.push(elem);
+        else if(i===arr1.length-1 && getPlayerInfo.length===0)  getPlayerInfo=404;
+    })
+
+    return getPlayerInfo;
+}
+
+//отримуємо футболіста з найбільшим показником голів в переможний рік
+function mostGoals(arr){
+    arr.sort((f1, f2) => f2.goalsScored - f1.goalsScored);
+    return `top-1 forward: ${arr[0].player} goals scored: ${arr[0].goalsScored}`;
+}
+
+//отримуємо гравця, який перемогав більше за всіх
+function mostWinner(arr,unicArr){
+    let lengthWin=0,winner;
+    unicArr.forEach((item) =>{
+        if(arr.filter(elem => (item===elem.player)).length> lengthWin){
+            lengthWin=arr.filter(elem => (item===elem.player)).length;
+            winner=item;
+        }
+    })
+    return `${winner} won ${lengthWin} times`;
+}
+
+//отримуємо наймолодшого переможця ЗМ
+function youngPlayer(arr){
+    const youngestGoldenBallWinner = arr.map(f => {
+        const birthYear = f.dateOfBirth.split('-')[0];
+        return { name: f.player, winningAge: f.year - birthYear };
+    })
+        .sort((f1, f2) => f1.winningAge - f2.winningAge)
+        .shift();
+
+    return `Younges golden ball winner is ${youngestGoldenBallWinner.name}. He won Golden Cup at the age of ${youngestGoldenBallWinner.winningAge}`;
+}
+
+
+
+//отримуємо масив унікальних ліг всіх переможців
+function titles(arr){
+    let setTitles= new Set();
+    arr.forEach(elem =>{
+        for( let title of elem.titlesWon){
+            setTitles.add(title);
+        }
+    })
+    return  [...setTitles].sort() ;
+}
+
+
+module.exports={getFootballers,getUnicFootballers,titles,mostGoals,mostWinner,youngPlayer,playerInfo};
--- routes/footballersRouter.js
+++ routes/footballersRouter.js
@@ -16,78 +16,36 @@ footballersRouter.get('/allWinners', async (request, response) => {
     response.send(footballersUnicList);
 });
 
+footballersRouter.get(`/PlayerInfo`, async (request,response) =>{
+    const {data: playersList}= await footballersController.getFootballers();
+    const playerSearch=request.query.player;
+    response.send(await footballersController.playerInfo(playersList,playerSearch));
+});
+
 
 footballersRouter.get(`/mostGoals`,async (request,response) =>{
 
-    const goals = await footballersController.getFootballers();
-    let goalsScored = 0, goalsScoredId = 0;
+    const {data: goals} = await footballersController.getFootballers();
+    response.send(await footballersController.mostGoals(goals));
+})
 
+footballersRouter.get(`/mostWinner`, async (request,response)  =>{
+    const {data: footballersList}= await footballersController.getFootballers();
+    const footballersUnicList=await footballersController.getUnicFootballers();
+    response.send(await footballersController.mostWinner(footballersList,footballersUnicList));
 
-    for(let i=0;i<goals.data.length;i++){
-        if(goals.data[i].goalsScored>goalsScored) {
-            goalsScored=goals.data[i].goalsScored;
-            goalsScoredId = i;
-        };
-    }
-
-    response.send(`top-1 forward: ${goals.data[goalsScoredId].player} goals scored: ${goals.data[goalsScoredId].goalsScored}` );
 })
 
 
 footballersRouter.get(`/youngPlayer`,async (request,response) => {
-    const footballersList = await footballersController.getFootballers();
-    let age = new Array();
-
-
-    footballersList.data.forEach(elem => {
-        let bd = elem.dateOfBirth.split('-');
-        if (age.length === 0) {
-            age = bd;
-            age.push(elem.player);
-        } else if (bd[0] > age[0] || bd[0] === age[0] && bd[1] > age[1] || bd[0] === age[0] && bd[1] === age[1] && bd[2] > age[2]) {
-            age = bd;
-            age.push(elem.player);
-        }
-
-    })
-
-
-    response.send(age.reverse());
+    const {data: footballersList} = await footballersController.getFootballers();
+    response.send(await footballersController.youngPlayer(footballersList));
 });
 
-footballersRouter.get(`/mostWinner`, async (request,response)  =>{
-    const footballersList= await footballersController.getFootballers();
-    const footballersUnicList=await footballersController.getUnicFootballers();
-    let lenWin=0;
-    let winner;
 
-
-
-    footballersUnicList.forEach((item) =>{
-        if(footballersList.data.filter(elem => (item===elem.player)).length>lenWin){
-            lenWin=footballersList.data.filter(elem => (item===elem.player)).length;
-            winner=item;
-        }
-    })
-
-    //я побачив як зробити краще, але жінка чекає мене дивитись серіал(((
-    response.send(`${winner} won ${lenWin} times`);
-
-})
-
-
-footballersRouter.get(`/PlayerInfo`, async (request,response) =>{
-    const playersList= await footballersController.getFootballers();
-    const playerSearch=request.query.player;
-    let getPlayerInfo= new Array();
-
-
-    playersList.data.forEach((elem,i,arr) =>{
-        if( elem.player.toLowerCase().includes(playerSearch))     getPlayerInfo.push(elem);
-        else if(i===arr.length-1 && getPlayerInfo.length===0)  response.send(404);
-        else if(i===arr.length-1 && getPlayerInfo.length>0)  response.send(getPlayerInfo);
-    })
+footballersRouter.get(`/titlesSort`, async (request,response) =>{
+    const {data: playersList}= await footballersController.getFootballers();
+    response.send(await footballersController.titles(playersList));
 });
-
 
 module.exports= {footballersRouter};
